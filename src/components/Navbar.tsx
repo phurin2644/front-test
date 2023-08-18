e2d7e5b53127c004fccf707eb3c98992ce7ecf79
phurin prasit <phurin@web1.dara.ac.th>
2023-08-18 23:59:46 +0700

    noting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,12 @@ function Navbar() {
                     <img src="src\img\icon.png " alt="" className="h-8 mt-4 mr-2" />
                     <h1 className="font-bold text-xl pt-4">Fast track</h1>
                 </div>
-                <div className="flex text-base space-x-4 h-full mx-9">
+                <div className="flex text-base space-x-4 h-full mx-10">
                     <h1 className="bg-transparent h-full pt-5 px-2 hover:bg-gradient-to-b from-green-pro to-green-c">Dashboard</h1>
                     <h1 className="bg-transparent h-full pt-5 px-2 hover:bg-gradient-to-b from-green-pro to-green-c">
                         Users
                     </h1>
-                    <div className=" px-7">
+                    <div className=" px-5">
                         <img src="src\img\profile.png" alt="" className="mt-2 w-12 h-12 rounded-full mx-auto object-cover" />
                     </div>
                 </div>
