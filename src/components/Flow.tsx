203c02dc0c449bd47a3101a7f622a8fb18da6bba
phurin prasit <phurin@web1.dara.ac.th>
2023-08-24 20:44:50 +0700

    create node

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,9 +1,50 @@
+import React, { useCallback, } from 'react';
+import ReactFlow, {
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    Connection,
+    Edge,
+    Panel,
+} from 'reactflow';
 
-import ReactFlow, { Controls, Background } from 'reactflow';
 
 import 'reactflow/dist/style.css';
 
+const initialNodes = [
+    { id: '1', position: { x: 200, y: 40 }, data: { label: '1' } },
+    { id: '2', position: { x: 200, y: 140 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2', animated: true }];
+
+const getNodeId = () => `randomnode_${+new Date()}`;
+
 function Flow() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+    const onConnect = useCallback((params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    const onAdd = useCallback(() => {
+        const newNode = {
+            id: getNodeId(),
+            data: { label: 'Added node' },
+            position: {
+                x: Math.random() * window.innerWidth - 100,
+                y: Math.random() * window.innerHeight,
+            },
+        };
+        setNodes((nds) => nds.concat(newNode));
+    }, [setNodes]);
+
+
     return (
         <>
             <div className='bg-slate-50 h-screen flex'>
@@ -15,9 +56,20 @@ function Flow() {
                         <button>ข้อมูล</button>
                     </div>
                 </div>
-                <ReactFlow>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    fitView
+                >
+                    <Panel position="top-right">
+                        <button onClick={onAdd}>add node</button>
+                    </Panel>
                     <Background></Background>
                     <Controls></Controls>
+
                 </ReactFlow>
             </div>
         </>
