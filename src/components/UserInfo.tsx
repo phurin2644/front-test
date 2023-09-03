d895bf6217190253f971d387af54cc740efe72fa
phurin prasit <phurin@web1.dara.ac.th>
2023-09-03 23:06:29 +0700

    make icon admin

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,11 +1,21 @@
-import { CircleCheck } from 'tabler-icons-react';
+import { CircleCheck, CircleLetterX } from 'tabler-icons-react';
 
 const Admin = () => (
-    <div className='bg-red'>
+    <div className='bg-green-400 h-6 w-6 flex items-center justify-center rounded-lg'>
       <CircleCheck
-        size={32}
+        size={20}
         strokeWidth={2}
-        color={'#e6b3b3'}
+        color={'#FFFFFF'}
+      />
+    </div>
+  );
+
+  const Normal = () => (
+    <div className='bg-red-400 h-6 w-6 flex items-center justify-center rounded-lg'>
+      <CircleLetterX
+        size={20}
+        strokeWidth={2}
+        color={'#FFFFFF'}
       />
     </div>
   );
@@ -15,21 +25,21 @@ function UserInfo() {
     return (
         <div>
             <div className='bg-slate-100 flex p-3 justify-between'>
-                <div className='flex w-64 bg-green-pro'>
+                <div className='flex w-64 '>
                     <h1 className='w-9'>1</h1>
                     <h1>phurin</h1>
                 </div>
-                <div className='flex w-48 bg-green-pro'>
+                <div className='flex w-48 '>
                     <h1>Cell Text</h1>
                 </div>
-                <div className='flex w-48 bg-green-pro' >
-                    <h1>Admin</h1>
+                <div className='flex w-48 ' >
                     <Admin />
+                    <Normal />
                 </div>
-                <div className='flex w-48 bg-green-pro'>
+                <div className='flex w-48 '>
                     <h1>วว/ดด/ปป</h1>
                 </div>
-                <div className='flex w-48 bg-green-pro'>
+                <div className='flex w-48 '>
                     <h1>Title</h1>
                 </div>
             </div>
