a432f3a0c6efa6b676b44d6b0c5e13d70c57f618
phurin prasit <phus.2644@gmail.com>
2023-09-04 00:15:42 +0700

    edit info

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,49 +1,53 @@
-import { CircleCheck, CircleLetterX } from 'tabler-icons-react';
+import { CircleCheck, CircleLetterX, Dots, FilePencil, Trash } from "tabler-icons-react";
 
 const Admin = () => (
-    <div className='bg-green-400 h-6 w-6 flex items-center justify-center rounded-lg'>
-      <CircleCheck
-        size={20}
-        strokeWidth={2}
-        color={'#FFFFFF'}
-      />
-    </div>
-  );
+  <div className="bg-green-400 h-6 w-6 flex items-center justify-center rounded-lg">
+    <CircleCheck size={20} strokeWidth={2} color={"#FFFFFF"} />
+  </div>
+);
 
-  const Normal = () => (
-    <div className='bg-red-400 h-6 w-6 flex items-center justify-center rounded-lg'>
-      <CircleLetterX
-        size={20}
-        strokeWidth={2}
-        color={'#FFFFFF'}
-      />
-    </div>
-  );
-  
+const Normal = () => (
+  <div className="bg-red-400 h-6 w-6 flex items-center justify-center rounded-lg">
+    <CircleLetterX size={20} strokeWidth={2} color={"#FFFFFF"} />
+  </div>
+);
 
 function UserInfo() {
-    return (
-        <div>
-            <div className='bg-slate-100 flex p-3 justify-between'>
-                <div className='flex w-64 '>
-                    <h1 className='w-9'>1</h1>
-                    <h1>phurin</h1>
-                </div>
-                <div className='flex w-48 '>
-                    <h1>Cell Text</h1>
-                </div>
-                <div className='flex w-48 ' >
-                    <Admin />
-                    <Normal />
-                </div>
-                <div className='flex w-48 '>
-                    <h1>วว/ดด/ปป</h1>
-                </div>
-                <div className='flex w-48 '>
-                    <h1>Title</h1>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="bg-slate-50 flex p-3 justify-between font-light">
+        <div className="flex w-64 ">
+          <h1 className="w-9">1</h1>
+          <h1>phurin</h1>
         </div>
-    );
+        <div className="flex justify-end">
+          <div className="flex w-40 ">
+            <h1>Cell Text</h1>
+          </div>
+          <div className="flex w-40 ">
+            <Admin />
+            <Normal />
+          </div>
+          <div className="flex w-40 ">
+            <h1>วว/ดด/ปป</h1>
+          </div>
+          <div className="flex w-44 justify-between">
+            <div className="flex">
+              <div className="mr-2 bg-slate-200 h-8 w-8 flex items-center justify-center rounded-md">
+                <FilePencil size={20} strokeWidth={2} color={"#79a1d2"} />
+              </div>
+              <div className="bg-slate-200 h-8 w-8 flex items-center justify-center rounded-md">
+                <Trash size={20} strokeWidth={2} color={"#79a1d2"} />
+              </div>
+            </div>
+
+            <div className="h-8 w-8 flex items-center justify-center ">
+              <Dots size={20} strokeWidth={2} color={"#79a1d2"} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default UserInfo
+export default UserInfo;
--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,56 +1,56 @@
-import { IconSearch } from '@tabler/icons-react';
-import { Input } from '@mantine/core';
-import UserInfo from '../components/UserInfo';
-
+import { IconSearch } from "@tabler/icons-react";
+import { Input } from "@mantine/core";
+import UserInfo from "../components/UserInfo";
 
 function Users() {
   return (
     <>
-      <div className='bg-slate-50 h-screen p-7'>
-        <div className='bg-white h-screen  mx-10 rounded-lg shadow-md px-10'>
-          <div className='flex justify-between p-4 pt-10'>
+      <div className="bg-slate-50 h-screen p-7">
+        <div className="bg-white h-screen  mx-10 rounded-lg shadow-md px-10">
+          <div className="flex justify-between p-4 pt-10">
             <div>
-              <h1 className='text-xl'>User</h1>
-              <h1 className='text-xs text-slate-500'>x users</h1>
+              <h1 className="text-xl">User</h1>
+              <h1 className="text-xs text-slate-500">x users</h1>
             </div>
-            <div className='justify-center'>
+            <div className="justify-center">
               <Input
-                className='w-96 '
-                icon={<IconSearch className='h-5' />}
+                className="w-96 "
+                icon={<IconSearch className="h-5" />}
                 variant="filled"
                 placeholder="Search"
                 radius="lg"
               />
             </div>
 
-            <button className='bg-green-pro rounded-lg p-2 text-sm text-white'>New Member</button>
+            <button className="bg-green-pro rounded-lg p-2 text-sm text-white">
+              New Member
+            </button>
           </div>
-          <div className='bg-slate-300 mt-7 flex p-3 justify-between'>
-            <div className='flex w-64 bg-green-pro'>
-              <h1 className='w-9'>No</h1>
+          <div className="bg-slate-300 mt-7 flex p-3 justify-between font-medium rounded-t-lg">
+            <div className="flex w-64 ">
+              <h1 className="w-9 ">No</h1>
               <h1>Username</h1>
             </div>
-            <div className='flex w-48 bg-green-pro'>
-              <h1>Department</h1>
-            </div>
-            <div className='flex w-48 bg-green-pro' >
-              <h1>Admin</h1>
-            </div>
-            <div className='flex w-48 bg-green-pro'>
-              <h1>Created</h1>
-            </div>
-            <div className='flex w-48 bg-green-pro'>
-              <h1>Title</h1>
+            <div className="flex justify-end">
+              <div className="flex w-40 ">
+                <h1>Department</h1>
+              </div>
+              <div className="flex w-40">
+                <h1>Admin</h1>
+              </div>
+              <div className="flex w-40 ">
+                <h1>Created</h1>
+              </div>
+              <div className="flex w-44 ">
+                <h1>Title</h1>
+              </div>
             </div>
           </div>
           <UserInfo />
-
         </div>
-
       </div>
-
     </>
-  )
+  );
 }
 
-export default Users
+export default Users;
