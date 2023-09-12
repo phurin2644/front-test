045e11265f39a074e88265391afdffdd9f6aabcc
JEXB <jippee.1211@gmail.com>
2023-09-12 19:16:55 +0700

    update

--- src/pages/InfoList.tsx
+++ src/pages/InfoList.tsx
@@ -1,8 +1,9 @@
 "use client";
-import InfoCard from "../components/InfoCards";
-import { IconSearch } from "@tabler/icons-react";
-import { Input } from "@mantine/core";
+// import InfoCard from "../components/InfoCards";
 import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import { useDisclosure } from "@mantine/hooks";
+import { Button, Modal } from "@mantine/core";
 
 const initialInfoCardsData = [
   {
@@ -20,45 +21,49 @@ const initialInfoCardsData = [
 ];
 
 function InfoList() {
-  const [infoCards, setInfoCards] = useState(initialInfoCardsData);
+  // const [infoCards, setInfoCards] = useState(initialInfoCardsData);
+  const [searchText, setSearchText] = useState("");
+  const [opened, { open, close }] = useDisclosure(false);
 
-  const handleCreateCard = () => {
-    const newCard = {
-      id: infoCards.length + 1,
-      title: "New Card",
-      content: "Content for New Card",
-      timestamp: "2023-09-10 00:00:00",
-    };
+  // const handleCreateCard = () => {
+  //   const newCard = {
+  //     id: infoCards.length + 1,
+  //     title: "New Card",
+  //     content: "Content for New Card",
+  //     timestamp: "2023-09-10 00:00:00",
+  //   };
 
-    setInfoCards([...infoCards, newCard]);
-  };
+  //   setInfoCards([...infoCards, newCard]);
+  // };
+
+  // เอาไปใส่ใน modal
 
   return (
     <div className="flex flex-col h-screen">
       <div className="flex justify-center items-center p-4">
         {/* Search Input */}
-        <Input
-          style={{ width: 600 }}
-          icon={<IconSearch className="h-5" />}
-          variant="filled"
-          placeholder="Search"
-          radius="lg"
-        />
-        {/* Create Button */}
+        <SearchBar value={searchText} onChange={setSearchText} />
+
+        {/* content Input */}
+        <Modal opened={opened} onClose={close} title="Authentication" centered>
+          {/* Modal content */}
+        </Modal>
+
         <div className="flex justify-end p-4">
-          <button
+          <Button
             style={{ width: 100 }}
-            onClick={handleCreateCard}
-            className="bg-slate-200 p-1 rounded-md"
+            // onClick={handleCreateCard}
+            className="bg-green-pro hover:bg-green-c p-1 rounded-md"
+            onClick={open}
           >
             Create
-          </button>
+          </Button>
         </div>
       </div>
 
-      <div className="bg-slate-50 p-1">
+      <div className="bg p-1">
         <div className="grid grid-cols-5 gap-4">
-          {infoCards.map((card) => (
+          {/* {infoCards.map((card) => (
             <div className="col-span-1 ml-4 mr-4">
               <InfoCard
                 id={card.id}
@@ -67,7 +72,7 @@ function InfoList() {
                 timestamp={card.timestamp}
               />
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
