99c500faf916adebbac68b1c64fa1d761439b2a7
JEXB <jippee.1211@gmail.com>
2023-09-12 19:52:33 +0700

    color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'green-pro': '#008C8C',
-        'bg-pro': '#F6FAFA',
-        'green-g': 'rgba(0, 161, 69, 0.2)',
-        'green-c': '#005454',
-        'green-light-3': '#E4F0EF',
-        'green-light-1': '#cce8e8',
-        'green-bar': '#48BABA'
+      colors: {
+        "green-pro": "#008C8C",
+        "bg-pro": "#F6FAFA",
+        "green-g": "rgba(0, 161, 69, 0.2)",
+        "green-c": "#005454",
+        "green-light-3": "#E4F0EF",
+        "green-light-1": "#cce8e8",
+        "green-bar": "#48BABA",
+        "green-Suscess-1": "#A2ECC2",
+        "green-Suscess-2": "#00A145",
+        "green-Suscess-3": "#006129",
       },
       backgroundColor: {
-        'default': '#F6FAFA',
+        default: "#F6FAFA",
       },
-      height:{
-        '670': 870,
-        '320': 420,
-        '450': 450,
-        '550': 500,
-        '966': 966,
-        '88': 88,
+      height: {
+        670: 870,
+        320: 420,
+        450: 450,
+        550: 500,
+        966: 966,
+        88: 88,
       },
-      width:{
-        '650': 650,
-        '550' : 550,
-        '1280': 1280
+      width: {
+        650: 650,
+        550: 550,
+        1280: 1280,
       },
-      
     },
   },
   plugins: [],
-}
-
+};
